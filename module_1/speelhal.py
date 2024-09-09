6bbbca35700f11debaf9d9d630d0a0388586bd51
RuoFeng207 <99081157@mydavinci.nl>
2024-09-09 12:02:32 +0200

    Update speelhal.py

--- module_1/speelhal.py
+++ module_1/speelhal.py
@@ -1,15 +1,16 @@
 #al het geld is in euro 
 toegang = 7.45
 personen = 4
-#koste voor 5 minuten
-koste_VIP_VR = 0.37
+#koste voor 1 minuten
+koste_VIP_VR = 0.074
 aantal_min = 45
 # totaal
 
 totaal_toegang = toegang*personen
 
 totaal_vr = koste_VIP_VR*aantal_min*personen
+print(totaal_vr)
 
-totaal_prijs =round(totaal_toegang*totaal_vr,2)
+totaal_prijs =round(totaal_toegang+totaal_vr,2)
 
-print(f"Je totaal prijs is {totaal_prijs} euro")
+print(f"Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {totaal_prijs} euro")
