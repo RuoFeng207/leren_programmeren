6718d04deabb491c9f2c24d0c57c49773f1efa6a
RuoFeng207 <99081157@mydavinci.nl>
2024-09-11 11:50:25 +0200

    Update feestlunch.py

--- module_1/feestlunch.py
+++ module_1/feestlunch.py
@@ -6,10 +6,10 @@ croissantjes_prijs = 0.39
 stokbroden_prijs = 2.78 
 Kortingsbon= 0.50
 #aantal
-aantal_croissantjes = 17
-aantal_stokbrood = 2
-aantal_kortingsbonnen = 3
+aantal_croissantjes = int(input("Hoeveel croissantjes wilt u: "))
+aantal_stokbrood = int(input("Hoeveel stokbrood wilt u: "))
+aantal_kortingsbonnen =int(input("Hoeveel kortingsbonnen heeft u: "))
 
 #totaal
 totaal = aantal_croissantjes*croissantjes_prijs + aantal_stokbrood*stokbroden_prijs - aantal_kortingsbonnen*Kortingsbon
-print(f"De feestlunch kost je bij de bakker {colored(totaal,'yellow', attrs=['bold'])} euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
+print(f"De feestlunch kost je bij de bakker {colored(totaal,'yellow', attrs=['bold'])} euro voor de {aantal_croissantjes} croissantjes en de {aantal_stokbrood} stokbroden als de {aantal_kortingsbonnen} kortingsbonnen nog geldig zijn!")
