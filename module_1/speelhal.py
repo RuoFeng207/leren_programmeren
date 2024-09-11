4daddd5c019bfc3057b3a56d2994700d47152429
RuoFeng207 <99081157@mydavinci.nl>
2024-09-11 11:50:28 +0200

    Update speelhal.py

--- module_1/speelhal.py
+++ module_1/speelhal.py
@@ -2,17 +2,17 @@ from termcolor import colored, cprint, COLORS
 
 #al het geld is in euro 
 toegang = 7.45
-personen = 4
+personen = int(input("Met hoeveel personen bent je: "))
 #koste voor 1 minuten
 koste_VIP_VR = 0.074
-aantal_min = 45
+aantal_min = int(input("Hoeveel minuten wilt je de vr bril gebruiken: "))
 # totaal
 
 totaal_toegang = toegang*personen
 
 totaal_vr = koste_VIP_VR*aantal_min*personen
-print(totaal_vr)
+
 
 totaal_prijs =round(totaal_toegang+totaal_vr,2)
 
-print(f"Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {colored(totaal_prijs,'yellow', attrs=['bold'])} euro")
+print(f"Dit geweldige dagje-uit met {personen} mensen in de Speelhal met {aantal_min} minuten VR kost je maar {colored(totaal_prijs,'yellow', attrs=['bold'])} euro")
