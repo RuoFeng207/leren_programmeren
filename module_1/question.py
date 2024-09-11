3685d70bd6765642887730906df591251b732e20
RuoFeng207 <99081157@mydavinci.nl>
2024-09-11 12:10:04 +0200

    Create question.py

---
+++ module_1/question.py
@@ -0,0 +1,5 @@
+naam=input("Hallo, wat is je naam? ")
+leeftijd=input(f"Goedemorge{naam}, hoe oud ben je? ")
+eten=input(f"En als {leeftijd} jarige, wat eet je het liefst? ")
+drinken= input(f"En wat drink je het liest bij {eten}? ")
+print(f"De {leeftijd} jarige {naam} drinkt het liefst {drinken} bij {eten}")
