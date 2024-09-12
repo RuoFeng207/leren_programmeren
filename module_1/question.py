95b47a2a93f2793b160dd2d33f5c9cb2d5740bf4
RuoFeng207 <99081157@mydavinci.nl>
2024-09-12 13:41:05 +0200

    Update question.py

--- module_1/question.py
+++ module_1/question.py
@@ -1,14 +1,14 @@
-# naam=input("Hallo, wat is je naam? ")
-# leeftijd=input(f"Goedemorge{naam}, hoe oud ben je? ")
-# eten=input(f"En als {leeftijd} jarige, wat eet je het liefst? ")
-# drinken= input(f"En wat drink je het liest bij {eten}? ")
-# print(f"De {leeftijd} jarige {naam} drinkt het liefst {drinken} bij {eten}")
-
-
-
 naam=input("Hallo, wat is je naam? ")
-tijd = input("Welk dag deel is het? ")
-leeftijd=input(f"Goede{tijd} {naam}, hoe oud ben je? ")
+leeftijd=input(f"Goedemorge{naam}, hoe oud ben je? ")
 eten=input(f"En als {leeftijd} jarige, wat eet je het liefst? ")
 drinken= input(f"En wat drink je het liest bij {eten}? ")
 print(f"De {leeftijd} jarige {naam} drinkt het liefst {drinken} bij {eten}")
+
+
+
+# naam=input("Hallo, wat is je naam? ")
+# tijd = input("Welk dag deel is het? ")
+# leeftijd=input(f"Goede{tijd} {naam}, hoe oud ben je? ")
+# eten=input(f"En als {leeftijd} jarige, wat eet je het liefst? ")
+# drinken= input(f"En wat drink je het liest bij {eten}? ")
+# print(f"De {leeftijd} jarige {naam} drinkt het liefst {drinken} bij {eten}")
