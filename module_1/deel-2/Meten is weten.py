2de68185a3dee370fa324f4a8352018e579f9605
RuoFeng207 <99081157@mydavinci.nl>
2024-09-13 09:19:26 +0200

    min en max

--- module_1/deel-2/Meten is weten.py
+++ module_1/deel-2/Meten is weten.py
@@ -5,9 +5,17 @@ if getal_1 > getal_2:
     print(f"{getal_1} is groter dan {getal_2}")
     max = getal_1
     print(f"Max getal is {max}")
-elif getal_2 >getal_1:
+    print(f"{getal_2} is kleiner dan {getal_1}")
+    Min = getal_2
+    print(f"Minimum getal is {Min}")
+  
+elif getal_1< getal_2:
     print(f"{getal_2} is groter dan {getal_1}")
     max = getal_2
     print(f"Max getal is {max}")
+    print(f"{getal_1} is kleiner dan {getal_2}")
+    Min = getal_1
+    print(f"Minimum getal is {Min}")
 else:
     print(f"{getal_1} is gelijk aan {getal_2}")
+    print("Er is geen maximum of minimum.")
