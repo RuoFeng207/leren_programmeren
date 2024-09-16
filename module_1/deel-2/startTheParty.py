9ce730042440c55e0b32e65d2f7bfa7dae1fec60
RuoFeng207 <99081157@mydavinci.nl>
2024-09-16 17:22:55 +0200

    Update startTheParty.py

--- module_1/deel-2/startTheParty.py
+++ module_1/deel-2/startTheParty.py
@@ -4,6 +4,9 @@ gasten = True
 drank = True
 chips = True
 
+
+gastheer= input("Wat is de naam van je gastheer/vrouw: ") 
+
 #Een feest moet minimaal gasten of een gastheer hebben.
 start_condition_1 = gastheer or gasten
 # Het feest kan alleen beginnen als de gastheer er is, tenzij er gasten, chips en drank zijn.
@@ -19,8 +22,9 @@ start_condition_6 = gasten and gastheer and drank
 
 
 if start_condition_1 and start_condition_2 and start_condition_3 and start_condition_4 and start_condition_5 and start_condition_6:
+    gastheer==("Ruo Feng")
     print('Start the Party')
 
 else:
+    gastheer==("Rudi")
     print('No Party')
-
