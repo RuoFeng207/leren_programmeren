c660f8be071acaae93f8ff1ab9331d4b4c38cd53
RuoFeng207 <99081157@mydavinci.nl>
2024-09-17 09:41:49 +0200

    Update startTheParty.py

--- module_1/deel-2/startTheParty.py
+++ module_1/deel-2/startTheParty.py
@@ -1,30 +1,30 @@
 
-gastheer = True
-gasten = True
-drank = True
+gastheer = False
+gasten = False
+drank = False
 chips = True
 
 
-gastheer= input("Wat is de naam van je gastheer/vrouw: ") 
+
 
 #Een feest moet minimaal gasten of een gastheer hebben.
 start_condition_1 = gastheer or gasten
-# Het feest kan alleen beginnen als de gastheer er is, tenzij er gasten, chips en drank zijn.
+# # Het feest kan alleen beginnen als de gastheer er is, tenzij er gasten, chips en drank zijn.
 start_condition_2 = gastheer or gasten and chips and drank
-# Een feest met chips, maar zonder drank kan niet beginnen.
-start_condition_3 = chips or chips and drank
-# Een feest met gasten kan niet beginnen als er geen chips en geen drank is.
+# # Een feest met chips, maar zonder drank kan niet beginnen.
+start_condition_3 = not chips and drank
+# # Een feest met gasten kan niet beginnen als er geen chips en geen drank is.
 start_condition_4 = gasten and chips and drank
-# Een gastheer kan een feest geven zonder chips en gasten, maar niet zonder drank.
+# # Een gastheer kan een feest geven zonder chips en gasten, maar niet zonder drank.
 start_condition_5 = gastheer and drank
-# Alleen chips is geen feest.
-start_condition_6 = gasten and gastheer and drank
+# # Alleen chips is geen feest.
+start_condition_6 = not chips 
 
 
 if start_condition_1 and start_condition_2 and start_condition_3 and start_condition_4 and start_condition_5 and start_condition_6:
-    gastheer==("Ruo Feng")
+    
     print('Start the Party')
 
 else:
-    gastheer==("Van pelt")
+    
     print('No Party')
