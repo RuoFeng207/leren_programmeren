c4c543485457a8c6e0da7a4afb8047c4c9f77526
RuoFeng207 <99081157@mydavinci.nl>
2024-09-17 16:05:03 +0200

    Create Studieadvies.py

---
+++ module_1/deel-2/Studieadvies.py
@@ -0,0 +1,37 @@
+from studieadviestext import *
+
+# inlijding
+print(STUDIEDOKTER_TITEL)
+AANTAL_WEKEN_VRAAG = input( 'Hoeveel weken ben je al bezig met de opleiding? ')
+print(f"{AANTAL_WEKEN_VRAAG} weken oke")
+
+#vraag 1
+print(f"Vraag 1: {COMPETENTIE_STELLING_1}")
+andwoord= input(f"{OPTIES}\n\n" )
+
+#vraag 2
+print(f"\nVraag 2: {COMPETENTIE_STELLING_2}")
+andwoord= input(f"{OPTIES}\n\n" )
+
+#vraag 3
+print(f"\nVraag 3: {COMPETENTIE_STELLING_3}")
+andwoord= input(f"{OPTIES}\n\n" )
+
+#vraag 4
+print(f"\nVraag 4: {COMPETENTIE_STELLING_4}")
+andwoord= input(f"{OPTIES}\n\n" )
+
+#vraag 5
+print(f"\nVraag 5: {COMPETENTIE_STELLING_5}")
+andwoord= input(f"{OPTIES}\n\n" )
+
+#vraag 6
+print(f"\nVraag 6: {COMPETENTIE_STELLING_6}")
+andwoord= input(f"{OPTIES}\n\n" )
+
+#vraag 7
+print(f"\nVraag 7: {COMPETENTIE_STELLING_7}")
+andwoord= input(f"{OPTIES}\n\n" )
+
+teller = andwoord+1
+print (andwoord)
