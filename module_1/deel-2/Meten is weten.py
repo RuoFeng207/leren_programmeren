9a7795d512b4ecc743fe213d06963a43b3294c67
RuoFeng207 <99081157@mydavinci.nl>
2024-09-20 10:03:10 +0200

    Update Meten is weten.py

--- module_1/deel-2/Meten is weten.py
+++ module_1/deel-2/Meten is weten.py
@@ -1,5 +1,5 @@
-getal_1 = input("Geef getal 1: ")
-getal_2 = input("Geef getal 2: ")
+getal_1 = int(input("Geef getal 1: "))
+getal_2 = int(input("Geef getal 2: "))
 
 if getal_1 > getal_2:
     print(f"{getal_1} is groter dan {getal_2}")
@@ -12,10 +12,12 @@ if getal_1 > getal_2:
 elif getal_1< getal_2:
     print(f"{getal_2} is groter dan {getal_1}")
     max = getal_2
-    print(f"Max getal is {max}")
+    # print(f"Max getal is {max}")
     print(f"{getal_1} is kleiner dan {getal_2}")
     Min = getal_1
-    print(f"Minimum getal is {Min}")
+    # print(f"Minimum getal is {Min}")
 else:
     print(f"{getal_1} is gelijk aan {getal_2}")
     print("Er is geen maximum of minimum.")
+print(f"Max getal is {max}")
+print(f"Minimum getal is {Min}")
