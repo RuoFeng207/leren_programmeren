341f7aef8431e34a39cdaed99fc356d3afadfa4c
RuoFeng207 <99081157@mydavinci.nl>
2024-09-29 18:17:33 +0200

    Update calculate.py

--- module_1/deel-4/calculate.py
+++ module_1/deel-4/calculate.py
@@ -1,5 +1,5 @@
 from test_lib import test, report
-from math_operations import increment, decrement, add, substract, multiply, divide
+from math_operations import increment, decrement, add, subtract, multiply, divide
 
 nr1 = 3
 nr2 = 11
@@ -12,28 +12,28 @@ result2 = multiply(nr3, 7)
 test('example', result1, result2)
 
 result1 = nr1 + nr2
-result2 = None
+result2 = add (nr1,nr2)
 test('add', result1,result2)
 
 result1 = (nr1 + nr2) * nr3
-result2 = None
+result2 = multiply(add(nr1,nr2),nr3)
 test('expression-1',result1, result2)
 
-result1 = nr4 / (nr3 - nr2)
-result2 = None
+result1 = nr4 / (nr3 - nr2) 
+result2 = divide (nr4,subtract(nr3,nr2))
 test('expression-2',result1, result2)
 
 result1 = (nr4 + nr1) / (nr3 - nr2)
-result2 = None
+result2 = divide(add(nr4,nr1),subtract(nr3,nr2))
 test('expression-3',result1, result2)
 
 result1 = nr1 + nr2 + nr3
-result2 = None
+result2 = add(add(nr1,nr2,),nr3)
 test('expression-4',result1, result2)
 
 # Bonusopdracht
 result1 = (nr1 - (nr4 - nr3)) / (nr2 + nr3)
-result2 = None
+result2 = divide(subtract(nr1,subtract(nr4,nr3)),add(nr2,nr3))
 test('expression-5', result1, result2)
 
 report()
