e44f712cd53e186c3664fff5b2ccbfc031d7900a
RuoFeng207 <99081157@mydavinci.nl>
2024-10-01 15:54:15 +0200

    Update tests.py

--- module_1/deel-4/tests.py
+++ module_1/deel-4/tests.py
@@ -1,16 +1,17 @@
 #import hier je functie "from [bestandsnaamZonderExtentie] import [functieNaam]"
 from test_lib import test, report
+from meten_is_weten_functie import num_grootst,num1,num2
 
-expected = '' #resultaat
-result = "" #roep hier je functie aan met 2 dezelfde getallen
+expected = num_grootst #resultaat
+result =  num1==num2 #roep hier je functie aan met 2 dezelfde getallen
 test('TEST nr1=nr2', expected, result)
 
-expected = '' #resultaat
-result = "" #roep hier je functie aan waar nr1 groter is dan nr2
+expected = num_grootst  #resultaat
+result =num1>num2  #roep hier je functie aan waar nr1 groter is dan nr2
 test('TEST nr1>nr2', expected, result)
 
-expected = '' #resultaat
-result ="" #roep hier je functie aan  waar nr1 kleiner is dan nr2
+expected = num_grootst #resultaat
+result= num2>num1 #roep hier je functie aan  waar nr1 kleiner is dan nr2
 test('TEST nr1<nr2', expected, result)
 
 if __name__ == "__main__":
