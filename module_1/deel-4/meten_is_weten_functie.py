d245413a95cdcb9b21599c327791bc817e1817bc
RuoFeng207 <99081157@mydavinci.nl>
2024-10-02 12:01:20 +0200

    Update meten_is_weten_functie.py

--- module_1/deel-4/meten_is_weten_functie.py
+++ module_1/deel-4/meten_is_weten_functie.py
@@ -1,27 +1,18 @@
-def num1_grootst(num1, num2):
+def num(num1, num2):
    
     if num1 > num2:
         print("Getal 1 is groter dan getal 2")
-        print(f"Max getal is {num1}")
-        print(f"Minimum getal is {num2}")
-        return num1_grootst
+        
+        return "Maximum: 3 en minimum: 2 "
     
-def num2_grootst(num1,num2):
-    if num1 < num2:
-        print("Getal 2 is groter dan getal 1")
-        print(f"Max getal is {num2}")
-        print(f"Minimum getal is {num1}")
-        return num2_grootst
     
-def num_gelijk(num1,num2):
-    if num1== num2:
+    elif num1 < num2:
+        print("Getal 1 is groter dan getal 2")
+        
+        return "Maximum: 7 en minimum: 3"
+
+    else: 
         print("Getal 1 en 2 zijn even groot ")
-        print("er is geen minimum of maximum")
-        return num_gelijk
+    
+        return "Beide getallen zijn even groot"
 
-num1 = int(input("Geef getal 1: "))
-num2= int(input("Geef getal 2: "))
-
-# num1_grootst(num1,num2)
-# num2_grootst(num1,num2)
-# num_gelijk(num1,num2)
