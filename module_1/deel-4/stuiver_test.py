7092efe7880302f99372750f1edcbb097d9b44d2
RuoFeng207 <99081157@mydavinci.nl>
2024-10-04 09:07:21 +0200

    Create stuiver_test.py

---
+++ module_1/deel-4/stuiver_test.py
@@ -0,0 +1,24 @@
+from test_lib import test, report
+from stuivers import afronden_5cent
+
+verwacht =bedrag =62.60
+krijgt = afronden_5cent (bedrag= 62.60)
+test ("Ik verwacht 62.60 euro",verwacht,krijgt)
+
+verwacht =bedrag =76.61
+krijgt = afronden_5cent (bedrag= 76.61 )
+test ("Ik verwacht 76.61 euro",verwacht,krijgt)
+
+verwacht =bedrag = 28.82
+krijgt = afronden_5cent (bedrag=28.82 )
+test ("Ik verwacht 28.82 euro",verwacht,krijgt)
+
+verwacht =bedrag =2.23
+krijgt = afronden_5cent (bedrag= 2.23)
+test ("Ik verwacht 2.23 euro",verwacht,krijgt)
+
+verwacht =bedrag =28.34
+krijgt = afronden_5cent (bedrag= 28.34)
+test ("Ik verwacht 28.34 euro",verwacht,krijgt)
+
+report()
