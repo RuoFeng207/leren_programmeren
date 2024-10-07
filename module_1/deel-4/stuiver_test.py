f76177416de71305a762268c5c4e9719cb30c8fa
RuoFeng207 <99081157@mydavinci.nl>
2024-10-07 11:32:34 +0200

    Update stuiver_test.py

--- module_1/deel-4/stuiver_test.py
+++ module_1/deel-4/stuiver_test.py
@@ -5,20 +5,20 @@ verwacht =bedrag =62.60
 krijgt = afronden_5cent (bedrag= 62.60)
 test ("Ik verwacht 62.60 euro",verwacht,krijgt)
 
-verwacht =bedrag =76.61
+verwacht =bedrag =76.60
 krijgt = afronden_5cent (bedrag= 76.61 )
-test ("Ik verwacht 76.61 euro",verwacht,krijgt)
+test ("Ik verwacht 76.60 euro",verwacht,krijgt)
 
-verwacht =bedrag = 28.82
+verwacht =bedrag = 28.80
 krijgt = afronden_5cent (bedrag=28.82 )
-test ("Ik verwacht 28.82 euro",verwacht,krijgt)
+test ("Ik verwacht 28.80 euro",verwacht,krijgt)
 
-verwacht =bedrag =2.23
+verwacht =bedrag =2.25
 krijgt = afronden_5cent (bedrag= 2.23)
-test ("Ik verwacht 2.23 euro",verwacht,krijgt)
+test ("Ik verwacht 2.25 euro",verwacht,krijgt)
 
-verwacht =bedrag =28.34
+verwacht =bedrag =28.35
 krijgt = afronden_5cent (bedrag= 28.34)
-test ("Ik verwacht 28.34 euro",verwacht,krijgt)
+test ("Ik verwacht 28.35w euro",verwacht,krijgt)
 
 report()
