2d41279d015d9409bbd6c5d0853416b27370771c
RuoFeng207 <99081157@mydavinci.nl>
2024-10-07 15:48:54 +0200

    Update data_zoeker.py

--- module_1/deel-4/data_zoeker.py
+++ module_1/deel-4/data_zoeker.py
@@ -1,30 +1,16 @@
+# toets_data has name:score combinations separated by a komma
+def get_value(toets_data: str, separator: str, position: int) -> str:
+    toets_data = 'Sofie:8,Emma:7,Ahmed:9,Daan:6,Lisa:8,Fatima:7,Ruben:9,Ayoub:6,Bram:6,Maria:7'
+    separator = ','
+    position= 8 # position of Bram, first position starts with 0
+    
+    splitted_strings = toets_data.split(separator) # string splits itself into collection of strings
+    if 0 <= position< len(splitted_strings):
+        value = splitted_strings[position] # read value at position of split_values
+    else:
+        value = None
+    
+    return(value) # prints: Bram:6
 
-# # # toets_data has name:score combinations separated by a komma
-toets_data = 'Sofie:8,Emma:7,Ahmed:9,Daan:6,Lisa:8,Fatima:7,Ruben:9,Ayoub:6,Bram:6,Maria:7'
-# separator = ','
-position= 8 # position of Bram, first position starts with 0
- 
-# splitted_strings = toets_data.split(separator) # string splits itself into collection of strings
-# if 0 <= position< len(splitted_strings):
-#   value = splitted_strings[position] # read value at position of split_values
-# else:
-#   value = None
- 
-# print(value) # prints: Bram:6
+print(get_value("Bram:6",",",9))
 
-
-def get_value(toets_data: list, separator: str, position: int) -> str:
-    toets_data.split(separator)
-    separator(",")
-    position (1)
-    return get_value
-
-toets_data = ('Sofie:8,Emma:7,Ahmed:9,Daan:6,Lisa:8,Fatima:7,Ruben:9,Ayoub:6,Bram:6,Maria:7')
-
-
-
-def test(tekst):
-    print((tekst).split())
-    return test
-
-test("Dit is een test")
