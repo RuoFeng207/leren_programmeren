5c27f9a868defd1920e005e59a5fbec5c0e20c87
RuoFeng207 <99081157@mydavinci.nl>
2024-10-08 09:59:29 +0200

    Update data_zoeker.py

--- module_1/deel-4/data_zoeker.py
+++ module_1/deel-4/data_zoeker.py
@@ -1,17 +1,35 @@
-# toets_data has name:score combinations separated by a komma
-toets_data = 'Sofie:8,Emma:7,Ahmed:9,Daan:6,Lisa:8,Fatima:7,Ruben:9,Ayoub:6,Bram:6,Maria:7'
-separator = ','
-position= 8 # position of Bram, first position starts with 0
- 
-splitted_strings = toets_data.split(separator) # string splits itself into collection of strings
-if 0 <= position< len(splitted_strings):
-  value = splitted_strings[position] # read value at position of split_values
-else:
-  value = None
- 
-print(value) # prints: Bram:6
+from test_lib import test, report
 
-splitted_strings=toets_data.split(separator)
+data = 'Sofie:8,Emma:7,Ahmed:9,Daan:6,Lisa:8,Fatima:7,Ruben:9,Ayoub:6,Bram:6,Maria:7'
+
 
 def get_value(data: str, separator: str, position: int) -> str:
-  data
+  waarde =data.split(separator)
+  if 0 <= position< len(waarde):
+    return waarde [position] # read value at position of split_values
+  else:
+    waarde = None
+  
+
+
+
+verwacht = "Bram:6"
+krijgt = get_value(data,",",8)
+test("naam verwacht Bram",verwacht,krijgt)
+
+verwacht = "Sofie:8"
+krijgt = get_value(data,",",0)
+test("naam verwacht Sopie",verwacht,krijgt)
+
+verwacht = "Emma:7"
+krijgt = get_value(data,",",1)
+test("naam verwacht Emma",verwacht,krijgt)
+
+verwacht = "Ahmed:9"
+krijgt = get_value(data,",",2)
+test("naam verwacht Ahmed",verwacht,krijgt)
+
+verwacht = "Daan:6"
+krijgt = get_value(data,",",3)
+test("naam verwacht Daan",verwacht,krijgt)
+report()
