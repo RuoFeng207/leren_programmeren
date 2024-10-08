5cd659bce1516911f66f54b06dfd773e4d9a159a
RuoFeng207 <99081157@mydavinci.nl>
2024-10-08 09:15:44 +0200

    Update data_zoeker.py

--- module_1/deel-4/data_zoeker.py
+++ module_1/deel-4/data_zoeker.py
@@ -1,16 +1,17 @@
 # toets_data has name:score combinations separated by a komma
-def get_value(toets_data: str, separator: str, position: int) -> str:
-    toets_data = 'Sofie:8,Emma:7,Ahmed:9,Daan:6,Lisa:8,Fatima:7,Ruben:9,Ayoub:6,Bram:6,Maria:7'
-    separator = ','
-    position= 8 # position of Bram, first position starts with 0
-    
-    splitted_strings = toets_data.split(separator) # string splits itself into collection of strings
-    if 0 <= position< len(splitted_strings):
-        value = splitted_strings[position] # read value at position of split_values
-    else:
-        value = None
-    
-    return(value) # prints: Bram:6
+toets_data = 'Sofie:8,Emma:7,Ahmed:9,Daan:6,Lisa:8,Fatima:7,Ruben:9,Ayoub:6,Bram:6,Maria:7'
+separator = ','
+position= 8 # position of Bram, first position starts with 0
+ 
+splitted_strings = toets_data.split(separator) # string splits itself into collection of strings
+if 0 <= position< len(splitted_strings):
+  value = splitted_strings[position] # read value at position of split_values
+else:
+  value = None
+ 
+print(value) # prints: Bram:6
 
-print(get_value("Bram:6",",",9))
+splitted_strings=toets_data.split(separator)
 
+def get_value(data: str, separator: str, position: int) -> str:
+  data
