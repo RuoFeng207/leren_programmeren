3b955acc0bedcc1a5940d69116f8ca11db17d9d2
RuoFeng207 <99081157@mydavinci.nl>
2024-10-11 09:48:08 +0200

    Update test.py

--- module_1/test.py
+++ module_1/test.py
@@ -1,3 +1,19 @@
-def test(Naam): #functie, parameter
-    print("Hallo",Naam) #print string parameter
-test("jan") #argument
+# def test(Naam): #functie, parameter
+#     print("Hallo",Naam) #print string parameter
+# test("jan") #argument
+
+
+# def getal(getal1: int, getal2:int)-> int:
+
+#     return getal1 +getal2
+# print(getal(getal1=1, getal2=2))
+
+
+# def tekst(hallo:str, aantal:int ):
+#     for i in range(aantal):
+#         print(hallo) 
+
+# tekst(hallo="hallo",aantal=3)
+# print("------------")
+# tekst(hallo="doei",aantal=6)
+
