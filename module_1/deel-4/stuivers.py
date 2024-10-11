2d453f6831fee8f90fd962fff2a7d87b62a99eca
RuoFeng207 <99081157@mydavinci.nl>
2024-10-11 09:47:58 +0200

    Update stuivers.py

--- module_1/deel-4/stuivers.py
+++ module_1/deel-4/stuivers.py
@@ -2,4 +2,5 @@
 # bedrag =round ((invul)*100/5)*5/100
 # print(bedrag)
 def afronden_5cent(bedrag):
-    return (round(bedrag*100/5)*5/100)
+    cent = 5
+    return (round(bedrag*100/cent)*cent/100)
