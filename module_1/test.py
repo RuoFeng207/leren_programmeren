fa1a97c1f3bf4cff20e9ddf61c5ddd9b1f2b79e0
RuoFeng207 <99081157@mydavinci.nl>
2024-10-12 19:27:07 +0200

    Update test.py

--- module_1/test.py
+++ module_1/test.py
@@ -9,15 +9,21 @@ def getal(getal1: int, getal2:int)-> int:
 print(getal(getal1=1, getal2=2))
 
 
-# def tekst(hallo:str, aantal:int ):
-#     for i in range(aantal):
-#         print(hallo) 
+def tekst(hallo:str, aantal:int ):
+    for i in range(aantal):
+        print(hallo) 
 
-# tekst(hallo="hallo",aantal=3)
-# print("------------")
-# tekst(hallo="doei",aantal=6)
+tekst(hallo="hallo",aantal=3)
+print("------------")
+tekst(hallo="doei",aantal=6)
 
 
 def aanval(defense:int,health:int,attack)->int:
     return defense, health,attack
 print(aanval(defense=1,health=2,attack=3))
+
+def kamer(deur_1:str):
+    return deur_1
+    
+  
+
