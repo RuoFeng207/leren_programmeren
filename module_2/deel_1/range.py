9d96a30f64983edd03a30d2b63a8f41dc9b5806e
RuoFeng207 <99081157@mydavinci.nl>
2024-10-19 17:17:08 +0200

    Update range.py

--- module_2/deel_1/range.py
+++ module_2/deel_1/range.py
@@ -3,6 +3,8 @@ lijst=[]
 for i in range(1,vraag+1):
     lengte =int(input(f"Hoelang moet lijst {i} zijn "))
     for x in range (1,lengte+1):
+        
         lijst.append(x)
+        
 
 print(lijst)
