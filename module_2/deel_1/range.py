07c5f9304d215863113629883e967f82878be0e3
RuoFeng207 <99081157@mydavinci.nl>
2024-10-21 11:49:13 +0200

    Update range.py

--- module_2/deel_1/range.py
+++ module_2/deel_1/range.py
@@ -1,9 +1,7 @@
-vraag = int(input("Hoeveel lijsten wilt u zien? "))
-lijst=[]
-for i in range(1,vraag+1):
-    lengte =int(input(f"Hoelang moet lijst {i} zijn "))
-    for x in range (1,lengte+1):
-        lijst.append(x)
-        
-
-print(lijst)
+vraag = int(input("Hoeveel lijsten wil je zien? "))
+lijst_1=[]
+lijst_2=[]
+for x in range(1,vraag+1):
+    
+    lijst_2.append(lijst_1)
+print(lijst_2)
