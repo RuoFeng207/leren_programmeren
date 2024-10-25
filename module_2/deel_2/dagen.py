4acb4cd288baf4a4c74252d0f6abe014caf48271
RuoFeng207 <99081157@mydavinci.nl>
2024-10-25 09:46:50 +0200

    Create dagen.py

---
+++ module_2/deel_2/dagen.py
@@ -0,0 +1,11 @@
+week = ("-maandag","-dinsdag","-woensdag","-donderdag","-vrijdag","-zaterdag","-zondag")
+
+
+print("Alledagen van de week zijn:")
+for a in (week):
+    print(a)
+print("")
+
+for b in (week):
+    print(b)
+print("")
