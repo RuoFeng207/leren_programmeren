c3c747bb125e0a99c7e0dfe8e308fc25f0f1d68d
RuoFeng207 <99081157@mydavinci.nl>
2024-10-31 12:26:46 +0100

    Update kaarten.py

--- module_2/deel_2/kaarten.py
+++ module_2/deel_2/kaarten.py
@@ -1,9 +1,17 @@
 import random
-for kaart in range(2):
-    kaarten=["aas",2,3,4,5,6,7,8,9,10,11,12,13]
-
-    kaart=random.choice(kaarten)
-    
-    print(kaart)
-    kaarten.remove(kaart)
-    print(kaarten)
+hand = []
+stapel = []
+teller=0
+# dek kaarten
+while True:
+    set = ["aas",2,3,4,5,6,7,8,9,10,"boer","vrouw","koning"]
+    nummer= random.choice(set)
+    simboolen =["harte", "klaveren","schoppen","ruiten"]
+    kaart_simbool = random.choice(simboolen)
+    kaart =(f"{kaart_simbool} {nummer}")
+    if kaart not in hand:
+        hand.append(kaart)
+        teller+=1
+    elif teller ==7:
+        break
+print(hand)
