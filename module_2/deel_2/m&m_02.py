191a2b16aca101f44cd61fdd756f2cb921196f9b
RuoFeng207 <99081157@mydavinci.nl>
2024-10-31 11:49:29 +0100

    Create m&m_02.py

---
+++ module_2/deel_2/m&m_02.py
@@ -0,0 +1,26 @@
+import random
+
+
+while True:
+    try:
+        hoeveel=(input("Hoeveel M&M's wil je? "))
+        hoeveel=int(hoeveel)
+        if hoeveel<0:
+            print("Je kan niet minder dan 0 M&M's toevoegen")
+        else:
+            break
+        
+    except:
+        print("Dit is geen hele M&M")
+zak ={}
+
+for x in range(hoeveel):
+    kleur = ["rood", "blauw", "groen", "geel", "bruin"]
+    kleur = random.choice(kleur)
+    if kleur in zak:
+        zak[kleur] +=1
+    else:
+        zak[kleur]= 1
+
+
+print(zak)
