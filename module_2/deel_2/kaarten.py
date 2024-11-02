e256f6de7460eef0865989c5d7ddf0c04efc3f07
RuoFeng207 <99081157@mydavinci.nl>
2024-11-02 12:11:28 +0100

    Update kaarten.py

--- module_2/deel_2/kaarten.py
+++ module_2/deel_2/kaarten.py
@@ -1,19 +1,18 @@
 import random
+symbool = ["harte", "klaveren","schoppen","ruiten"]
+set = ["aas","2","3","4","5","6","7","8","9","10","boer","vrouw","koning"]
+stapel = 54
+dek=[]
 for keer in range (4):
-    for kaart in range(1,14):
-        if kaart ==1:
-            kaart = "aas"
-        elif kaart==11:
-            kaart ="boer"
-        elif kaart ==12:
-            kaart= "vrouw"
-        elif kaart == 13:
-            kaart ="koning"
-        if keer ==0:
-            print(f"harten {kaart}")
-        elif keer ==1:
-            print(f"schoppen {kaart}")
-        elif keer ==2:
-            print(f"klaveren {kaart}")
-        else:
-            print(f"ruiten {kaart}")
+    for kaart in set:
+        kaart =(f"{symbool[keer]} {kaart}")
+        dek.append(kaart)
+        
+for x in range(1,8):
+    keus = random.choice(dek)
+    hand= (f"kaart {x}: {keus}")
+    stapel= stapel-1
+    dek.remove(keus)
+    print(hand)
+print("")
+print(f"dek ({stapel} kaarten): {dek}")
