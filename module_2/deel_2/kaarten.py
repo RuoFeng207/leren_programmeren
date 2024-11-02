dc41aed49e1965611919e0609d59d7ec6060ad48
RuoFeng207 <99081157@mydavinci.nl>
2024-11-02 10:05:35 +0100

    Update kaarten.py

--- module_2/deel_2/kaarten.py
+++ module_2/deel_2/kaarten.py
@@ -3,15 +3,12 @@ hand = []
 stapel = []
 teller=0
 # dek kaarten
-while True:
-    set = ["aas",2,3,4,5,6,7,8,9,10,"boer","vrouw","koning"]
+for x in range (7):
+    set = ["aas","2","3","4","5","6","7","8","9","10","boer","vrouw","koning"]
     nummer= random.choice(set)
     simboolen =["harte", "klaveren","schoppen","ruiten"]
     kaart_simbool = random.choice(simboolen)
     kaart =(f"{kaart_simbool} {nummer}")
     if kaart not in hand:
         hand.append(kaart)
-        teller+=1
-    elif teller ==7:
-        break
-print(hand)
+    print(kaart)
