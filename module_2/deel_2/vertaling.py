373d0976f74d43e592e4db3ed1735f3d5c501be9
RuoFeng207 <99081157@mydavinci.nl>
2024-11-05 12:08:38 +0100

    Create vertaling.py

---
+++ module_2/deel_2/vertaling.py
@@ -0,0 +1,20 @@
+# In een klein dorpje woonde een oude man die prachtige houten speelgoed maakte. Op een dag ontdekte hij een vergeten zolder vol met oude, magische houtsoorten. Terwijl hij aan het werk was, kwam er een levendig paard tevoorschijn uit een van de blokken. Het paard nam de oude man mee op een avontuurlijke reis
+# verhaal= input("voer hier je verhaal in: ")
+# split_verhaal = verhaal.split()
+# print(split_verhaal)
+# woord_1 = split_verhaal[2] 
+# print(woord_1)
+# d = {"":woord_1,"":"groot"}
+# print(d)
+# print(split_verhaal)
+
+# Using curly braces
+my_dict = {
+    'name': 'Alice',
+    'age': 30,
+    'city': 'New York'
+}
+
+# Using the dict() constructor
+my_dict2 = dict(name='Bob', age=25, city='Los Angeles')
+print(my_dict2)
