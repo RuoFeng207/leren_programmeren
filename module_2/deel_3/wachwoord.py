f421d801566b810865d38b732098ce4b6df50630
RuoFeng207 <99081157@mydavinci.nl>
2024-11-13 18:52:06 +0100

    Update wachwoord.py

--- module_2/deel_3/wachwoord.py
+++ module_2/deel_3/wachwoord.py
@@ -1,12 +1,19 @@
-
-poging = 5
+import time
 while True:
-    wachtwoord = input("Voer uw wachtwoord in: ")
+    poging = 5
+    while True:
+        wachtwoord = input("Voer uw wachtwoord in: ")
+        if wachtwoord =="humtidumtie217":
+            print("Oke kom er maar in")
+            break
+        else:
+            poging=poging-1
+            if poging>0 and poging<6:
+                print(f"dit wachtwoord is in corect je hebt nog {poging} pogingen")
+            else:
+                print("helaas je wachtwoord is incorect probeer het over 24 uur")
+                time.sleep(86400)
+                break
     if wachtwoord =="humtidumtie217":
-        print("Oke kom er maar in")
-    elif poging>1 and poging<6:
-        poging=poging-1
-        print(f"dit wachtwoord is in corect je hebt nog {poging} pogingen")
-    else:
-        print("helaas je wachtwoord is incorect probeer het over 24 uur")
         break
+          
