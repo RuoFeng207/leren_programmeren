36f7f658572bd4e77f0437ff38ac4792c77d0339
RuoFeng207 <99081157@mydavinci.nl>
2024-11-13 11:18:30 +0100

    Update kaarten.py

--- module_2/deel_2/kaarten.py
+++ module_2/deel_2/kaarten.py
@@ -1,31 +1,11 @@
 import random
-# symbool = ["harte", "klaveren","schoppen","ruiten"]
-# set = ["aas","2","3","4","5","6","7","8","9","10","boer","vrouw","koning"]
-# kaarten = 0
-# dek=[]
-# for keer in range (4):
-#     for kaart in set:
-#         kaarten=kaarten+1
-#         kaart =(f"{symbool[keer]} {kaart}")
-#         dek.append(kaart)
-        
-        
-# for x in range(1,8):
-#     keus = random.choice(dek)
-#     hand= (f"kaart {x}: {keus}")
-#     kaarten=kaarten-1
-#     dek.remove(keus)
-#     print(hand)
-# print("")
-# print(f"dek {kaarten} kaarten): {dek}")
-# # aanpassen
 
-symbool = ["harte", "klaveren","schoppen","ruiten"]
-set = ["aas","2","3","4","5","6","7","8","9","10","boer","vrouw","koning"]
+symbool = ["harte", "klaveren","schoppen","ruiten","vierkantje"]
+set = ["aas","2","3","4","5","6","7","8","9","10","boer","vrouw","koning","timmerman"]
 stapel = 0
 dek=[]
 #dek genereren
-for keer in range (4):
+for keer in range (5):
  for kaart in set:
     stapel=stapel+1
     kaart =(f"{symbool[keer]} {kaart}")
