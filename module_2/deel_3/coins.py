e90eff452d0e04441fc25ca3c4fd9ff91f455c85
RuoFeng207 <99081157@mydavinci.nl>
2024-11-15 11:58:31 +0100

    Update coins.py

--- module_2/deel_3/coins.py
+++ module_2/deel_3/coins.py
@@ -3,7 +3,9 @@
 # purpose of program: 
 # structure of program: 
 
-coinValues = [50,20,10,500,200,100] # waarde munt
+lijst = []
+lijst_2=[]
+coinValues = [500,200,100,50,20,10] # waarde munt
 
 toPay = int(float(input('Amount to pay: '))* 100) # vraag hoeveel er betaald word
 paid = int(float(input('Paid amount: ')) * 100) # vraag hoeveel je betaald hebt
@@ -16,11 +18,22 @@ while change > 0 and len(coinValues) > 0: # als de verandering 0 is en de conval
 
   if nrCoins > 0: # als nr coins groter is dan 0 zet regel 17=20 in werking
     print('return maximal ', nrCoins, ' coins of ', coinValue, ' cents!' ) # hoeveelheid munten en waarden printen
+    lijst.append(nrCoins)
+    lijst_2.append(coinValue)
     nrCoinsReturned = int(input('How many coins of ' + str(coinValue) +  ' cents did you return? ')) # vraag hoeveel je terug betaald heb
     change -= nrCoinsReturned * coinValue #change =change - nrcoinsreturned
+    
 print(f"paid back ")
+while len(lijst):
+  vervang =lijst.pop(0)
+  vervang_2=lijst_2.pop(0)
+  print(f"coins {vervang}:{vervang_2} cent")
+  
 
 if change > 0: #change groter dan 0 zet regel 22-25 in werking
   print('Change not returned: ', str(change) + ' cents') # ptint hoeveel er nog niet betaald is 
 else:
   print('done')
+
+
+
