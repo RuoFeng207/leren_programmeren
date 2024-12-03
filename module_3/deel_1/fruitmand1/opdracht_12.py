bfa3d7d0d1f3abfb952716f963eb8a695895818b
RuoFeng207 <99081157@mydavinci.nl>
2024-12-03 11:24:26 +0100

    opdracht 12 fruitmand

--- module_3/deel_1/fruitmand1/opdracht_12.py
+++ module_3/deel_1/fruitmand1/opdracht_12.py
@@ -1,5 +1,6 @@
 from fruitmand import fruitmand
-lijst=[]
+min_lengte = 0
+
 kleur = {
     "red": "rood",
     "yellow": "geel",
@@ -7,9 +8,19 @@ kleur = {
     "orange": "oranje",
     "brown": "bruin"
 }
-for fruit in fruitmand:
-    naam=(fruit["name"])
-    kleur=(fruit["color"])
 
-    lengte=len(naam)
-    neuw =print(naam, lengte)
+for fruit in fruitmand:
+    naam = fruit["name"]
+    kleur2 = fruit["color"]
+    gewicht= fruit["weight"]
+    lengte = len(naam)
+
+
+    if lengte > min_lengte:
+        min_lengte = lengte
+        N_kleur= kleur[kleur2]
+        langste_fruit = naam
+        N_gewicht=gewicht/1000
+       
+       
+print(f"De {langste_fruit} ({min_lengte} letters) een {N_kleur} kleur en een gewicht van {N_gewicht} kg")
