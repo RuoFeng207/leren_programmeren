5a452c61864e2c0dd2d81c69660e7b9cec1c3c5a
RuoFeng207 <99081157@mydavinci.nl>
2024-12-09 11:25:14 +0100

    Update boodschappenlijst.py

--- module_3/deel_2/boodschappenlijst.py
+++ module_3/deel_2/boodschappenlijst.py
@@ -1,34 +1,49 @@
-hoeveel =[]
-product=[]
+hoeveel = []
+product = []
 
 while True:
-    artiekel = input("Welk artikel wilt u toevoegen? ")
-    if not artiekel.isalpha():
-        print("U kunt hier geen cijfer invoeren.")
-        print("")
+    while True:
+        artikel = input("Welk artikel wilt u toevoegen? ").lower()
+        if not artikel.isalpha():
+            print("U kunt hier geen cijfer invoeren.")
+            print("")
+        else:
+            break
+    print("")
+
+    if artikel in product:
+        index = product.index(artikel) 
+        while True:
+            try:
+                aantal = int(input(f"Hoeveel wilt u daarvan toevoegen? U heeft er al {hoeveel[index]} van. "))
+                hoeveel[index] += aantal 
+                break
+            except ValueError:
+                print("U kunt hier alleen hele getallen invoeren.")
+                print("")
     else:
+        product.append(artikel)  
+        while True:
+            try:
+                aantal = int(input("Hoeveel wilt u daarvan toevoegen? ").lower())
+                hoeveel.append(aantal)  
+                break
+            except ValueError:
+                print("U kunt hier alleen hele getallen invoeren.")
+                print("")
+
+    while True:
+        nogeens = input("Wilt u nog iets bestellen? ").lower()
+        if nogeens == "ja" or nogeens == "nee":
+            break
+        else:
+            print("")
+            print("U kunt alleen maar ja of nee invullen.")
+    print("")
+    
+    if nogeens == "nee":
         break
-print("")
-product.append(artiekel)
 
-for a in product:
-    print(a)
-
-
-
-while True:
-    try:
-        aantal = int(input("Hoeveel wilt u daarvan toevoegen? "))
-        break
-    except ValueError:
-        print("U kunt hier alleen hele getallen invoeren.")
-        print("")
-
-hoeveel.append(aantal)
-
-for x in (hoeveel):
-    print(x)
-
-
-
-
+print("========Bon=======")
+for i in range(len(product)):
+    print(f"{hoeveel[i]} x {product[i]}")
