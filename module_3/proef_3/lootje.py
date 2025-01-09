01eb32c2b5f8e4708557cc8aaf592eda1f4a1ac7
RuoFeng207 <99081157@mydavinci.nl>
2025-01-09 11:18:14 +0100

    Update lootje.py

--- module_3/proef_3/lootje.py
+++ module_3/proef_3/lootje.py
@@ -2,7 +2,10 @@ import random
 
 deelnemers = []
 lootjes = []
-for x in range(3):
+teller =0
+extra = ''
+while True:
+    teller+=1
     while True:
         while True:
             naam=input("Wat is jou naam? ")
@@ -19,7 +22,20 @@ for x in range(3):
             deelnemers.append(naam)
             lootjes.append(naam)
             break
-for y in range(3):
+    if teller>=3:
+        while True:
+            extra= input("Wil je nog een naam invoeren? ")
+            extra.lower
+       
+            if extra =="ja" or extra=="nee":
+                break
+            else:
+                print("Je kan alleen maar ja of nee invoeren")
+    if extra =="nee":
+        print("Oke")
+        break
+
+for y in range(teller):
     while True:
         kies1= random.choice(deelnemers)
         kies2 = random.choice(lootjes)
