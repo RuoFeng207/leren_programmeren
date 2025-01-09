597162549d949d0b465e712e40f763e5cb7aca1b
RuoFeng207 <99081157@mydavinci.nl>
2025-01-09 10:29:42 +0100

    Update lootje.py

--- module_3/proef_3/lootje.py
+++ module_3/proef_3/lootje.py
@@ -1,37 +1,33 @@
-deelnemers =[]
+import random
+
+deelnemers = []
+lootjes = []
 for x in range(3):
     while True:
-        naam = input("Wat is je naam? ")
-        try:
-            naam= float(naam)
-            print("Je naam mag geen cijfers bevatten")
-        except ValueError:
-            deelnemers.append(naam)
-        
         while True:
-            if naam in deelnemers:
-                print("Deze naam zit al in de lijst.")
-            else:
-                deelnemers.append(naam)
+            naam=input("Wat is jou naam? ")
+            if naam.isalpha():
+                naam = naam.capitalize()
+                print(naam)
                 break
-
+            else:
+                print("Je naam mag geen cijfers of tekens bevatten.")
+                print("")
+        if naam in deelnemers:
+            print("Deze naam zit al in de lijst.")
+        else:
+            deelnemers.append(naam)
+            lootjes.append(naam)
+            break
+for y in range(3):
+    while True:
+        kies1= random.choice(deelnemers)
+        kies2 = random.choice(lootjes)
         
-
-
-#     while True:
-#         while True:
-#             naam=input("Wat is jou naam? ")
-#             if naam.isalpha():
-#                 naam = naam.capitalize()
-#                 print(naam)
-#                 break
-#             else:
-#                 print("Je naam mag geen cijfers of tekens bevatten.")
-#                 print("")
-        # if naam in deelnemers:
-        #     print("Deze naam zit al in de lijst.")
-        # else:
-        #     deelnemers.append(naam)
-        #     break
-
-# print(deelnemers)
+        
+        if kies1 != kies2:
+            deelnemers.remove(kies1)
+            lootjes.remove(kies2)
+            trekt = print(f"{kies1} heeft {kies2}")
+            break
+        
