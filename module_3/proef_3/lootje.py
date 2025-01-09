bdda4cb3cc966d9e0c82b253ada7bb6b2b8f7774
RuoFeng207 <99081157@mydavinci.nl>
2025-01-09 09:32:04 +0100

    Update lootje.py

--- module_3/proef_3/lootje.py
+++ module_3/proef_3/lootje.py
@@ -1,19 +1,37 @@
 deelnemers =[]
 for x in range(3):
     while True:
-        while True:
-            naam=input("Wat is jou naam? ")
-            if naam.isalpha():
-                naam = naam.capitalize()
-                print(naam)
-                break
-            else:
-                print("Je naam mag geen cijfers of tekens bevatten.")
-                print("")
-        if naam in deelnemers:
-            print("Deze naam zit al in de lijst.")
-        else:
+        naam = input("Wat is je naam? ")
+        try:
+            naam= float(naam)
+            print("Je naam mag geen cijfers bevatten")
+        except ValueError:
             deelnemers.append(naam)
-            break
+        
+        while True:
+            if naam in deelnemers:
+                print("Deze naam zit al in de lijst.")
+            else:
+                deelnemers.append(naam)
+                break
 
-print(deelnemers)
+        
+
+
+#     while True:
+#         while True:
+#             naam=input("Wat is jou naam? ")
+#             if naam.isalpha():
+#                 naam = naam.capitalize()
+#                 print(naam)
+#                 break
+#             else:
+#                 print("Je naam mag geen cijfers of tekens bevatten.")
+#                 print("")
+        # if naam in deelnemers:
+        #     print("Deze naam zit al in de lijst.")
+        # else:
+        #     deelnemers.append(naam)
+        #     break
+
+# print(deelnemers)
