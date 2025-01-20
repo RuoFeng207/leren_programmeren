f441ca7650b5ff30ca249346918bf09cc1477c82
RuoFeng207 <99081157@mydavinci.nl>
2025-01-20 10:01:50 +0100

    Update gegevens_uitbreiden.py

--- module_4/deel_1/gegevens_uitbreiden.py
+++ module_4/deel_1/gegevens_uitbreiden.py
@@ -1,14 +1,31 @@
 def gegevens():
-    naam = input("Wat is je naam? ").capitalize()
+    lijst=[]
     while True:
-        try: 
-            leeftijd = int(input("Wat is je leeftijd "))
+        naam = input("Wat is je naam? ").capitalize()
+        while True:
+            try: 
+                leeftijd = int(input("Wat is je leeftijd "))
+                break
+            except ValueError:
+                print("Je mag aleen hele cijfers doen")
+        gegeven= {"naam":naam, "leeftijd":leeftijd}
+        deel=(f"{gegeven["naam"]} is {gegeven["leeftijd"]} jaar")
+        lijst.append(deel)
+        while True:
+            nogeens=input("Wil je nog een naam invoeren? (Ja/Nee) ").lower()
+            print("")
+            if nogeens =="nee":
+                break
+            elif nogeens=="ja":
+                break
+            else:
+                print("Je kunt aleen ja of nee invoeren")
+        if nogeens == "nee":
             break
-        except ValueError:
-            print("Je mag aleen hele cijfers doen")
-    d= {"naam":naam,
-    "leeftijd":leeftijd}
-    print(f"{d["naam"]} is {d["leeftijd"]} jaar")
-    return d
+
+    while lijst:
+        show = lijst.pop(0)
+        print(show)
+    return gegeven
 
 gegevens()
