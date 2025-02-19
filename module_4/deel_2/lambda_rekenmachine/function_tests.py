cb99fda3d49a7270ebb12c79633d3bc64fcc9e36
RuoFeng207 <99081157@mydavinci.nl>
2025-02-19 13:02:49 +0100

    update

--- module_4/deel_2/lambda_rekenmachine/function_tests.py
+++ module_4/deel_2/lambda_rekenmachine/function_tests.py
@@ -1,2 +1,15 @@
 from functions_rekenen_2 import *
 from test_lib import test, report
+expected = 10
+calculated = addition(nummer_1= 5,nummer_2=5)
+test("resultaat:",expected,calculated)
+
+expected = 5
+calculated = subtraction(nummer_1= 10,nummer_2=5)
+test("resultaat:",expected,calculated)
+
+expected = 10
+calculated = addition(nummer_1= 5,nummer_2=5)
+test("resultaat:",expected,calculated)
+
+report()
--- module_4/deel_2/rekenmachine/functions_rekenen.py
+++ module_4/deel_2/rekenmachine/functions_rekenen.py
@@ -1,19 +1,26 @@
 def addition(nummer_1:float, nummer_2:float):
     add =nummer_1+nummer_2
     return add
+get_som = lambda nummer_1,nummer_2: f"{nummer_1} + {nummer_2} = {nummer_1+nummer_2}"
+print(get_som(5,3))
 
 def subtraction(nummer_1:float, nummer_2:float):
     subtract = nummer_1 - nummer_2
     return subtract
+get_som = lambda nummer_1,nummer_2: f"{nummer_1} - {nummer_2} = {nummer_1-nummer_2}"
+print(get_som(5,3))
 
 def multiplication(nummer_1:float, nummer_2:float):
     multipli = nummer_1 * nummer_2
     return multipli
+get_som = lambda nummer_1,nummer_2: f"{nummer_1} x {nummer_2} = {nummer_1*nummer_2}"
+print(get_som(5,3))
 
 def division(nummer_1:float, nummer_2:float):
     divis = nummer_1/nummer_2
     return divis
-
+get_som = lambda nummer_1,nummer_2: f"{nummer_1} : {nummer_2} = {nummer_1/nummer_2:.1f}"
+print(get_som(5,3))
 # print(addition(10,3))
 # print(subtraction(10,3))
 # print(multiplication(10,3))
