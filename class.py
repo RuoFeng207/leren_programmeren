e1254075e78664af89b60e94da1d93b8dc9c9704
RuoFeng207 <99081157@mydavinci.nl>
2026-02-26 09:36:25 +0100

    python classes
    Dit was een introductie in python classes

---
+++ class.py
@@ -0,0 +1,6 @@
+class boom:
+    def __init__(self,name, treetype):
+        self.name = name
+        self.treetype =treetype
+b= boom("amirikaanse eik" , "eik")
+print (f"name: {b.name} type: {b.treetype}")
